62f80c58cc1fb947b7a0dc20eda8ff544d510b3b
J3r3mC <j.corbeilla@outlook.fr>
2023-06-09 13:06:38 +0200

    ADD connexion to database

---
+++ routes/user.routes.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const app = require("./app");
+const User = require("./model/user");
+
+app.post("/register", (req, res) => {
+
+});
+
+app.post("/login", (req, res) => {
+
+});
+
