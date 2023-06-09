b447cf058770f7ce036201103c1f2cbde0ab7fb9
J3r3mC <j.corbeilla@outlook.fr>
2023-06-09 18:03:50 +0200

    FIX login

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,11 +2,11 @@ module.exports = app => {
     const users = require("../controllers/users.controller");
     const auth = require("../middleware/auth");
     var router = require("express").Router();
-
     app.post("/register", users.create);
 
     app.post("/login", users.signIn);
-
+ 
+    //route for test middleware login
     app.post("/welcome", auth, (req, res) => {
         res.status(200).send("Welcome 🙌 ");
       });
