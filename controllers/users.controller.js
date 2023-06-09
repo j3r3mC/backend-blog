db073cb55fe70d7d069ecebd4447c4ebbf12ba84
J3r3mC <j.corbeilla@outlook.fr>
2023-06-09 20:46:16 +0200

    FIX CRUD users

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -39,40 +39,36 @@ exports.create = async (req, res) => {
 };
 
 exports.signIn = async (req, res) => {
-    // Our login logic starts here
-    try {
-       // Get user input
-       const { email, password } = req.body;
-   
-       // Validate user input
-       if (!(email && password)) {
-         res.status(400).send("All input is required");
-       }
-       // Validate if user exist in our database
-       const user = await User.findOne({ email });
-   
-       if (user && (await bcryptjs.compare(password, user.password))) {
-         // Create token
-         const token = jwt.sign(
-           { user_id: user._id, email },
-           process.env.TOKEN_KEY,
-           {
-             expiresIn: "2h",
-           }
-         );
-   
-         // save user token
-         user.token = token;
-   
-         // user
-         res.status(200).json(user);
-       }
-       
-     } catch (err) {
-       console.log(err);
-       res.status(400).send("Invalid Credentials");
-     }
-     // Our register logic ends here      
+    // Get user input
+    const { email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+    // Validate if user exist in our database
+    const user = await User.findOne({ email });
+
+    if (user && (await bcryptjs.compare(password, user.password))) {
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      // save user token
+      user.token = token;
+
+      // user
+      res.status(200).json(user);
+    } else {
+      res.status(400).send("Invalid Credentials");
+    }
+    
+ 
 }
 
 exports.getAllUsers = (req, res) => {
